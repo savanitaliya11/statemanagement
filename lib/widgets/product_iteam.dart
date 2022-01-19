83b725c62b813d80b6f5072c39d394d1078f4cba
savan <savanitaliya11@gmail.com>
2022-01-19 17:00:15 +0530

    cachednetworkimage

--- lib/widgets/product_iteam.dart
+++ lib/widgets/product_iteam.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,25 +8,37 @@ class ProductItem extends StatelessWidget {
   String? id;
   String? title;
   String? imageUrl;
-  ProductItem({this.id, this.title, this.imageUrl});
+  String? price;
+  ProductItem({this.id, this.title, this.imageUrl, this.price});
 
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(
-        imageUrl!,
+      child: CachedNetworkImage(
         fit: BoxFit.cover,
+        imageUrl: imageUrl!,
       ),
       footer: GridTileBar(
         backgroundColor: Colors.black54,
         title: Text(
-          title!,
+          price!,
           textAlign: TextAlign.center,
         ),
         trailing:
             IconButton(onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
         leading: IconButton(
             onPressed: () {}, icon: Icon(Icons.favorite_border_rounded)),
+      ),
+      header: GridTileBar(
+        backgroundColor: Colors.black12,
+        title: Text(title!),
+        leading: Text(
+          id!,
+          style: TextStyle(
+              color: Colors.deepOrange,
+              fontWeight: FontWeight.bold,
+              fontSize: 18),
+        ),
       ),
     );
   }
