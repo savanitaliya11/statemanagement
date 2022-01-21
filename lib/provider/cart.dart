dfef7a85a28428c4aaaf9b5508ad480f92f4df76
savan <savanitaliya11@gmail.com>
2022-01-21 16:54:04 +0530

    create new cart provider with cart iteam class for add data in shopping cart

---
+++ lib/provider/cart.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+
+class CartIteam {
+  final String? id;
+  final String? title;
+  final double? price;
+  final int? quntity;
+
+  CartIteam({this.id, this.title, this.price, this.quntity});
+}
+
+class Cart with ChangeNotifier {
+  //main cart iteam
+  Map<String, CartIteam>? _iteams;
+
+  Map<String, CartIteam> get iteams {
+    return _iteams!;
+  }
+
+  void addCart(String id, String title, double price) {
+    if (_iteams!.containsKey(id)) {
+      _iteams!.update(
+          id,
+          (existingIteam) => CartIteam(
+              id: id,
+              quntity: existingIteam.quntity! + 1,
+              title: title,
+              price: price));
+    } else {
+      _iteams!.putIfAbsent(
+          id,
+          () => CartIteam(
+              id: DateTime.now().toString(),
+              price: price,
+              title: title,
+              quntity: 1));
+    }
+  }
+}
