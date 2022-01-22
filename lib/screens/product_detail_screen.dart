b4910277c31ab4f69414c32dcb826602524b6bae
savan <savanitaliya11@gmail.com>
2022-01-22 16:32:39 +0530

    Showing data in to details screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, non_constant_identifier_names
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,8 +10,9 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)?.settings.arguments as String;
-    final productData = Provider.of<Products>(context).findById(productId);
-    print('==============>>>>>>>>>>>>.{$productData}');
+    final productData =
+        Provider.of<Products>(context, listen: false).findById(productId);
+
     // .iteams
     // .firstWhere((element) => element.id == productId);
     return Scaffold(
@@ -20,11 +22,51 @@ class ProductDetailScreen extends StatelessWidget {
           style: TextStyle(fontFamily: 'Lato'),
         ),
       ),
-      body: SafeArea(
-          child: Text(
-        productData.description!,
-        style: TextStyle(fontFamily: 'Lato'),
-      )),
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 300,
+                child: CachedNetworkImage(
+                  imageUrl: productData.imageUrl!,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                productData.title!,
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 2, left: 10, right: 10),
+                child: Text(
+                  productData.description!,
+                  softWrap:
+                      true, //if des is too long then it will automatically take new line
+                  style: TextStyle(color: Colors.black87, fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                productData.price!.toString(),
+                style: TextStyle(color: Colors.black87, fontSize: 15),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
